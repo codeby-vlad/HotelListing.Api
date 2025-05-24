e82a8b2d741345d1c8df230ddff920630eb10dd8
Vladyslav <jricher58@gmail.com>
2025-05-24 17:20:27 +0200

    Added Countries Endpoint

--- HotelListing.Api/Program.cs
+++ HotelListing.Api/Program.cs
@@ -1,5 +1,6 @@
 using HotelListing.Api.data;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.OpenApi.Models;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -10,18 +11,38 @@ builder.Services.AddDbContext<HotelListingDbContext>(options =>
 
 builder.Services.AddControllers();
 
-// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
-builder.Services.AddOpenApi();
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen(c =>
+{
+    c.SwaggerDoc("v1", new OpenApiInfo { Title = "HotelListing API", Version = "v1" });
+});
+
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("AllowAll", policy =>
+    {
+        policy.AllowAnyOrigin()
+              .AllowAnyMethod()
+              .AllowAnyHeader();
+    });
+});
 
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
-    app.MapOpenApi();
+    app.UseSwagger();                      
+    app.UseSwaggerUI(c =>                  
+    {
+        c.SwaggerEndpoint("/swagger/v1/swagger.json", "HotelListing API v1");
+        c.RoutePrefix = "swagger";
+    });
 }
 
 app.UseHttpsRedirection();
+
+app.UseCors("AllowAll");
 
 app.UseAuthorization();
 
